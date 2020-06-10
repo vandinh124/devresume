f645bd5b6f504d223b69bad5aed19d036c850634
vandinh124 <vanvdinh124@gmail.com>
2020-06-10 10:30:33 -0700

    finish backend

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs')
+const auth = require('../middleware/auth.middleware')
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check, validationResult} = require('express-validator');
+
+const User = require('../models/User.model');
+//@route  Get api/auth
+//@desc     Test route
+//@access   Public
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.error(err.messsage);
+        res.status(500).send('Server error');
+    }
+});
+
+//@route  POST api/users
+//@desc     Test route
+//@access   Public
+
+router.post(
+    '/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists()
+    ],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400)
+            .json({errors: errors.array()})
+        }
+
+        const {email, password} = req.body;
+        try {
+            // See if user exists
+            let user = await User.findOne({ email });
+            if(!user){
+                return res.status(400)
+                .json({ errors: [{message: 'Invalid Credentials'}]});
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(!isMatch){
+                return res.status(400).json({errors: [{message: "Invalid Credetials"}]})
+            }
+            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                {expiresIn: 360000},
+                (err, token)=> {
+                    if(err) throw err;
+                    res.json({token});
+                });
+        }catch(err){
+            console.error(err.message);
+            res.status(500).send('Server error')
+        }
+
+        
+});
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
-
+const connectDB = require('./config/db')
 const app = express();
 
+//connect DB
+connectDB();
+
+//Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('API Running'))
+
+//Define routes
+app.use('/api/users', require('./routes/user.routes'));
+app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/profile', require('./routes/profile.routes'));
+app.use('/api/posts', require('./routes/posts.routes'));
 
 const PORT = process.env.PORT || 8000;
 
