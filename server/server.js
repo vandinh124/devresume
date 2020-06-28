f7baf45fa514f2a45098bef5d27e84d233eb035c
vandinh124 <vanvdinh124@gmail.com>
2020-06-28 01:13:50 -0700

    prepared for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 //connect DB
@@ -8,13 +9,20 @@ connectDB();
 //Init Middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.send('API Running'))
-
 //Define routes
 app.use('/api/users', require('./routes/user.routes'));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/profile', require('./routes/profile.routes'));
 app.use('/api/posts', require('./routes/posts.routes'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 8000;
 
